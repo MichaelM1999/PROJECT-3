21919dbc7d5d7767500d67830241b02d249958fe
Dylan Ferragame <50226788+Dylster99@users.noreply.github.com>
2019-11-14 15:59:03 -0700

    Update contact.js

--- client/src/components/pages/contact.js
+++ client/src/components/pages/contact.js
@@ -6,8 +6,13 @@ const Contact = () => {
 
         <div>
             <Header/>
+            <div id = contactpagetext>
             <h1>Here are some ways to contact us</h1>
-            <p><button class = "contactbutton"><a class = "linkemail" href="https://github.com/MichaelM1999/PROJECT-3">Visit our Github Repo</a></button></p>
+            <p>
+            Thank you for visiting our site! You may have already seen that some functions of our app aren't working if done quickly, this is due to us using a free api for this project making it difficult to display all the info we want with limited calls. If you are having problems with a certain function try slowing down to let our api catch up or report this issue to one of the developers below.
+            </p>
+            </div>
+            <p><button class = "contactbutton"><a class = "linkemail" href="https://github.com/Dylster99/Project3final">Visit our Github Repo</a></button></p>
 
             <p><button class = "contactbutton"><a class = "linkemail" href = "mailto:dyl6999@hotmail.com?subject = Feedback&body = Message">
             dyl6999@hotmail.com
